c9186de2c6713aa84bd2678323ccd09340c80364
lluanps <lluanps@gmail.com>
2023-05-01 16:29:25 -0300

    Criacao e configuracao do projeto

---
+++ ecommerce/src/main/java/com/luan/ecommerce/model/Produto.java
@@ -0,0 +1,30 @@
+package com.luan.ecommerce.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private  Integer id;
+	
+	private String nome;
+	private String descricao;
+	private BigDecimal preco;
+	
+}
