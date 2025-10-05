c18f8abc689c6b8dd37cbcdb9a03f53068397640
lluanps <lluanps@gmail.com>
2025-10-05 11:53:02 -0300

    Selecionando um atributo da entidade como retorno da consulta

---
+++ ecommerce/src/test/java/com/luan/ecommerce/jpql/BasicoJPQLTest.java
@@ -0,0 +1,44 @@
+package com.luan.ecommerce.jpql;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.jupiter.api.Test;
+
+import com.luan.ecommerce.EntityManagerTest;
+import com.luan.ecommerce.model.Cliente;
+import com.luan.ecommerce.model.Pedido;
+
+public class BasicoJPQLTest  extends EntityManagerTest {
+	
+	@Test
+	public void buscarPorIdentificador() {
+		TypedQuery<Pedido> typedQuery = entityManager
+				.createQuery("select p from Pedido p where p.id = 1", Pedido.class);
+		
+		Pedido pedido = typedQuery.getSingleResult();
+		
+		assertNotNull(pedido);
+	}
+
+	@Test
+	public void selcionarUmAtributoParaRetorno() {
+		String jpql = "select p.nome from Produto p";
+		
+		TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+		List<String> list = typedQuery.getResultList();
+		
+		assertTrue(String.class.equals(list.get(0).getClass()));
+		
+		String jpqlCliente = "select p.cliente from Pedido p";
+		TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
+		List<Cliente> listCliente = typedQueryCliente.getResultList();
+		
+		assertTrue(Cliente.class.equals(listCliente.get(0).getClass()));
+	}
+	
+}
